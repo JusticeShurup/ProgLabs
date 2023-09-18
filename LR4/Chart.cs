ab88021ec900dfd3680de159696872a74bf3c250
JusticeShurup <mixas_2004@mail.ru>
2023-09-18 23:10:47 +0500

    LR4 First Commit

---
+++ LR4/Chart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR4
+{
+    public class Chart
+    {
+        public float CalculateIntegral(int a, int b)
+        {
+            return (float)(b * b) / 2 - (float)(a * a) / 2;
+        }
+
+    }
+}
--- LR4/Program.cs
+++ LR4/Program.cs
@@ -2,9 +2,39 @@
 {
     public class Program 
     {
+        public static void FirstTask()
+        {
+            Chart chart = new Chart();
+            Console.Write("Введите левую границу a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите правую границу b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Интеграл функции y = x от a до b равен {0}", chart.CalculateIntegral(a, b));
+
+        }
+
+        public static void SecondTask() 
+        {
+            Money money = new(100, 100);
+            Console.WriteLine($"Количество купюр: {money[2]}\nНоминал этих купюр: {money[1]}");
+            Console.WriteLine($"Сумма денег: {money.value}");
+            money++;
+            Console.WriteLine($"Количество купюр: {money[2]}\nНоминал этих купюр: {money[1]}");
+            Console.WriteLine($"Сумма денег: {money.value}");
+            money--;
+            Console.WriteLine($"Количество купюр: {money[2]}\nНоминал этих купюр: {money[1]}");
+            Console.WriteLine($"Сумма денег: {money.value}");
+            Console.WriteLine(!money);
+            money += 500;
+            Console.WriteLine($"Количество купюр: {money[2]}\nНоминал этих купюр: {money[1]}");
+            Console.WriteLine($"Сумма денег: {money.value}");
+        }
+
         public static void Main(string[] args)
         {
-            
+            FirstTask();
+            SecondTask();
         }
     }
 }
