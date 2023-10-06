e03dea8d5402338840108cb95177f7d963a165a4
Grishin Michael <69842449+JusticeShurup@users.noreply.github.com>
2023-10-06 17:53:26 +0500

    LR6 update

---
+++ LR6/Point2D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR6
+{
+    public class Point2D : APair
+    {
+        public int x;
+        public int y;
+
+        public Point2D()
+        {
+            x = 0;
+            y = 0;
+        }
+        public Point2D(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Координата x = {x}\nКоордината y = {y}");
+        }
+    }
+}
---
+++ LR6/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LR6
+{ 
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+
+            Point2D point2D = new Point2D(5, 5);
+            point2D.Print();
+
+        }
+    }
+}
