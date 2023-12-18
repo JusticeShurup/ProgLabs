ae99a8b390b616d441a0c2acedc4b279af328bf1
Grishin Michael <69842449+JusticeShurup@users.noreply.github.com>
2023-12-18 11:53:30 +0500

    LR 13 done

---
+++ LR13/LR13/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace LR13
+{
+
+    public class MyThread
+    {
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+
+
+        public MyThread(int row, int col) 
+        { 
+            Row = row;
+            Column = col;
+        }
+
+        public void ThreadMain()
+        {
+            int[,] matrix1 = new int[Row, Column];
+            int[,] matrix2 = new int[Row, Column];
+        
+            for (int i = 0; i < Row; i++)
+            {
+                for (int j = 0; j < Column; j++)
+                {
+                    matrix1[i, j] = new Random().Next(9);
+                    matrix2[i, j] = new Random().Next(9);
+                }
+            }
+
+
+            int[,] resultMatrix = new int[Row, Column];
+            
+            Console.Write("Результирующая матрица");
+            for (int i = 0; i < Row; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < Column; j++)
+                {
+                    resultMatrix[i, j] = matrix1[i, j] + matrix2[i, j];
+                    Console.Write($"{resultMatrix[i, j]} ");
+                
+                }
+            }
+
+
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new MyThread(5, 5);
+
+            var t3 = new Thread(obj.ThreadMain);
+            t3.Start();
+
+            t3.Join();
+            
+        }
+    }
+}
